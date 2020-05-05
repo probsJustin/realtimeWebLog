9bcac807adb30b054cf6ad11b3159c7f043d7335
Justin Hagerty <wheelsmanx@gmail.com>
2020-05-05 07:38:38 -0400

    added a bunch of cool stuff to it

--- app.py
+++ app.py
@@ -8,16 +8,27 @@ from flask_socketio import SocketIO, emit, join_room, leave_room, \
 async_mode = None
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 thread_lock = Lock()
+
+def line(funcString):
+    try:
+        return funcString + '\n'
+    except Exception as error:
+        return funcString
+
+def append(funcString, funcStringApp):
+    try:
+        funcString = funcString + funcStringApp
+        return funcString
+    except Exception as error:
+        return str(error)
 
 
 @app.route('/')
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
-
 
 @app.route('/log')
 def log():
@@ -25,6 +36,23 @@ def log():
                   {'data': request.args.get('body'), 'count': 0},
                   namespace='/test')
     return str(request.args.get('body'))
+
+
+@app.route('/apiTest_GET')
+def apiTest_GET():
+    returnObject = ""
+    try:
+        returnObject += line("USER: " + request.args.get("weblogUser"))
+    except Exception as error:
+        returnObject += line("USER: NO USER FOUND FOR PARAM weblogUser")
+
+    for x in request.headers.keys():
+        returnObject += line(str(x) + " : " + str(request.headers.get(x)))
+
+    socketio.emit('my_response',
+                  {'data': returnObject, 'count': 0},
+                  namespace='/test')
+    return returnObject
 
 
 if __name__ == '__main__':
--- readme.md
+++ readme.md
@@ -1,3 +1,13 @@
 # Real Time Web Socket Log 
 
-Hacked together real time log - will add dynatrace tracing as well
+A really simple real time socketio log. Basically you have a python flask server set up that uses socketio and takes requests at /log and then you pass /log a parameter called body and it will post it in the websocket log. 
+
+I will be expanding this out a bit more to include request analysis as well. 
+
+### Endpoints: 
+ - /log
+	Takes a parameter of body and then logs it in the websocket log.
+ - /apiTest_GET
+	Takes a parameter of 'weblogUser' that sets the userID in the web log and this endpoint prints the headers of the request to the log.
+	
+ 
